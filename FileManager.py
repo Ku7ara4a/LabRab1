66ad8153d48b8a12a47cdcce2443f51bb14830dd
Ku7ara4a <arxwoodj@gmail.com>
2025-10-19 18:01:13 +0300

    Смена курса на работу с адресами, возможно сейчас начнется работа со складом и системой поиска заказов между складами, так же был добавлен небольшой рисунок городка , где будут доставки со складов и созданы при помощи имеющейя программы два файла с жителями и адресами складов и домов

--- FileManager.py
+++ FileManager.py
@@ -1,4 +1,4 @@
-from main import *
+from classes import *
 import xml.etree.ElementTree as ET
 
 '''Saving users to XML'''
@@ -11,9 +11,7 @@ def save_users_to_xml(users, filename):
         ET.SubElement(user_elem, "surname").text = user.surname
         ET.SubElement(user_elem, "email").text = user.email
         ET.SubElement(user_elem, "password").text = user.password
-        addresses_elem = ET.SubElement(user_elem, "addresses")
-        for address in user.addresses:
-            ET.SubElement(addresses_elem, "address").text = address
+        ET.SubElement(user_elem, "address_name").text = user.address
     tree = ET.ElementTree(root)
     tree.write(filename, encoding="utf-8", xml_declaration=True)
     print(f"Пользователи сохранены в файл {filename}")
@@ -29,13 +27,80 @@ def load_users_from_xml(filename):
         surname = user_elem.find("surname").text
         email = user_elem.find("email").text
         password = user_elem.find("password").text
+        address_name = user_elem.find("address_name").text
         user = User(user_id, name, surname, email, password)
-        addresses_elem = user_elem.find("addresses")
-        if addresses_elem is not None:
-            for addr_elem in addresses_elem.findall("address"):
-                user.addresses.append(addr_elem.text)
         users.append(user)
     return users
+"""Load single user from XML"""
+def load_single_users_from_xml(filename,user_id: int):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    user : User
+    for user_elem in root.findall("user"):
+        loaded_user_id = int(user_elem.find("user_id").text)
+        if user_id == loaded_user_id:
+            name = user_elem.find("name").text
+            surname = user_elem.find("surname").text
+            email = user_elem.find("email").text
+            password = user_elem.find("password").text
+            address_name = user_elem.find("addresses").text
+            user = User(loaded_user_id, name, surname, email, password, address_name)
+    if user:
+        print(f"Пользователь с айди {user_id} найден")
+        return user
+    else:
+        print(f"Пользователь с айди {user_id} не существует")
+        return None
 
+def load_addresses_from_xml(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    addresses = []
+    for address_elem in root.findall("address"):
+        adress_name = address_elem.find("name").text
+        address_pos = float(address_elem.find("position").text)
+        address_spec = address_elem.find("spec").text
+        address = Address(adress_name, address_pos,address_spec)
+        addresses.append(address)
+    return addresses
 
+def saving_addresses_to_xml(addresses, filename):
+    root = ET.Element("addresses")
+    for address in addresses:
+        address_elem = ET.SubElement(root, "address")
+        ET.SubElement(address_elem, "name").text = address.name
+        ET.SubElement(address_elem, "position").text = str(address.pos)
+        ET.SubElement(address_elem, "spec").text = address.spec
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding="utf-8", xml_declaration=True)
+    print(f"Адресса сохранены в файл {filename}")
 
+def load_single_addresses_from_xml(filename,address_name : str):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    address : Address = None
+    for address_elem in root.findall("address"):
+        loaded_address_name = address_elem.find("name").text
+        if loaded_address_name == address_name:
+            address_pos = float(address_elem.find("position").text)
+            spec = address_elem.find("spec").text
+            address = Address(loaded_address_name, address_pos,spec)
+    if address:
+        print(f"Искомый адресс с названием {address_name} найден")
+        return address
+    else:
+        print(f"Искомый адресс с названием {address_name} не существует")
+        return None
+
+'''Testing and uploading some random persons and houses from MiniHomeTown.png'''
+addresses = [Address("H1",10,"Living"),
+             Address("H2",20,"Living"),
+             Address("H3",30,"Living"),
+             Address("WH1",0,"Warehouse"),
+             Address("WH2",40,"Warehouse"),]
+saving_addresses_to_xml(addresses, "addresses.xml")
+
+users = [User(1,"Denis","Chaban","arxwoodj@gmail.com","notgonnasay","H1"),
+         User(2,"Ivan","Ivanov","IvanovIvanIvanovich@ivan.ru","Vanya","H2"),
+         User(3,"IDK","IDKOVICH","ILostMyCreativity@sad.com","Smth","H3")]
+save_users_to_xml(users, "users.xml")
--- classes.py
+++ classes.py
@@ -1,49 +1,64 @@
-"""Classes for Products"""
-class Product: #Абстрактный класс продуктов
-    def __init__(self,product_id: int, name: str, price: float):
+#Address Class
+class Address:
+    def __init__(self, name: str, pos: float,spec: str):
+        self.name = name
+        self.pos = pos
+        self.spec = spec
+#Product Abstract Class
+class Product:
+    def __init__(self, product_id: int, name: str, price: float):
         self.product_id = product_id
         self.name = name
         self.price = price
+
     def update_price(self, new_price: float):
         self.price = new_price
 
-class Electronic(Product): #Абстрактный класс электроники
-    def __init__(self, product_id: int, name: str, price:float, model : str, power: float ):
-        super().__init__(product_id, name,price)
+
+class Electronic(Product):  #Абстрактный класс электроники
+    def __init__(self, product_id: int, name: str, price: float, model: str, power: float):
+        super().__init__(product_id, name, price)
         self.model = model
         self.power = power
 
-class Smartphone(Electronic): #Класс телефонов
-    def __init__(self, product_id: int, name: str,price:float, model : str, power: float, memory:int, battery:int, os:str ):
-        super().__init__(product_id,name,price,model,power)
+
+class Smartphone(Electronic):  #Класс телефонов
+    def __init__(self, product_id: int, name: str, price: float, model: str, power: float, memory: int, battery: int,
+                 os: str):
+        super().__init__(product_id, name, price, model, power)
         self.os = os
         self.battery = battery
         self.memory = memory
 
-class Laptop(Electronic): #Класс ноутбуков
-    def __init__(self, product_id: int, name: str,price:float, model : str, power: float, ram: int, processor:str,storage:int,screen_size: str):
-        super().__init__(product_id,name,price,model,power)
+
+class Laptop(Electronic):  #Класс ноутбуков
+    def __init__(self, product_id: int, name: str, price: float, model: str, power: float, ram: int, processor: str,
+                 storage: int, screen_size: str):
+        super().__init__(product_id, name, price, model, power)
         self.ram = ram
         self.processor = processor
         self.storage = storage
         self.screen_size = screen_size
 
+
 """Classes for Storage System"""
+
+
 class ShelfCell:
-    def __init__(self,cell_id:int, max_quantity:int):
+    def __init__(self, cell_id: int, max_quantity: int):
         self.cell_id = cell_id
         self.max_quantity = max_quantity
         self.quantity = 0
         self.product = None
 
-    def store(self,product: Product,quantity: int) -> bool:
+    def store(self, product: Product, quantity: int) -> bool:
         if self.product is None:
             if quantity <= self.max_quantity:
                 self.product = product
                 self.quantity = quantity
                 return True
             return False
-        elif self.product.product_id==product.product_id:
+        elif self.product.product_id == product.product_id:
             if self.quantity + quantity <= self.max_quantity:
                 self.quantity += quantity
                 return True
@@ -51,7 +66,7 @@ class ShelfCell:
         else:
             return False
 
-    def retrieve(self,quantity:int) -> bool:
+    def retrieve(self, quantity: int) -> bool:
         if self.quantity >= quantity:
             self.quantity -= quantity
             if self.quantity == 0:
@@ -62,99 +77,102 @@ class ShelfCell:
     def is_available(self) -> bool:
         return self.quantity < self.max_quantity
 
+
 class Shelf:
     def __init__(self, shelf_id: int, num_cells: int, cell_capacity: int):
         self.shelf_id = shelf_id
-        self.cells = [ShelfCell(i+1,cell_capacity) for i in range(num_cells)]
+        self.cells = [ShelfCell(i + 1, cell_capacity) for i in range(num_cells)]
 
-    def add_product(self,product: Product,quantity: int) -> int:
+    def add_product(self, product: Product, quantity: int) -> int:
         for cell in self.cells:
             if cell.is_available() and (cell.product is None or cell.product.product_id == product.product_id):
                 space_available = cell.max_quantity - cell.quantity
-                space_to_store = min(space_available,quantity)
+                space_to_store = min(space_available, quantity)
                 if space_to_store > 0:
-                    cell.store(product,space_to_store)
+                    cell.store(product, space_to_store)
                     quantity -= space_to_store
                 if quantity == 0:
                     return True
         return quantity == 0
 
-    def get_product(self,product: Product,quantity:int) -> bool:
+    def get_product(self, product: Product, quantity: int) -> bool:
         for cell in self.cells:
             if cell.product and cell.product.product_id == product.product_id:
                 if quantity == 0:
                     break
-                take = min(cell.quantity,quantity)
+                take = min(cell.quantity, quantity)
                 cell.retrieve(take)
                 quantity -= take
         return quantity == 0
 
-    def all_available(self,product: Product) -> int:
+    def all_available(self, product: Product) -> int:
         total_amount = 0
         for cell in self.cells:
             if cell.product and cell.product.product_id == product.product_id:
                 total_amount += cell.quantity
         return total_amount
 
+
 class Warehouse:
-    def __init__(self,warehouse_id: int, name : str):
+    def __init__(self, warehouse_id: int, name: str,address:Address):
         self.warehouse_id = warehouse_id
         self.name = name
-        self.shelves  = []
+        self.shelves = []
 
-    def add_shelf(self,shelf: Shelf):
+    def add_shelf(self, shelf: Shelf):
         self.shelves.append(shelf)
 
-    def get_shelfnum(self,id_of_shelf: int) -> int:
+    def get_shelfnum(self, id_of_shelf: int) -> int:
         i = 0
         for shelf in self.shelves:
             if shelf.shelf_id == id_of_shelf:
                 return i
-            i+=1
+            i += 1
         return 0
 
-    def get_stock(self,product: Product) -> int:
+    def get_stock(self, product: Product) -> int:
         total_amount = 0
         for shelf in self.shelves:
-                total_amount += shelf.all_available(product)
+            total_amount += shelf.all_available(product)
         return total_amount
 
-    def find_product(self,product: Product):
+    def find_product(self, product: Product):
         cells_of_product = []
         for shelf in self.shelves:
             for cell in shelf.cells:
                 if cell.product and cell.product.product_id == product.product_id:
-                    cells_of_product.append((shelf.shelf_id,cell.cell_id,cell.quantity))
+                    cells_of_product.append((shelf.shelf_id, cell.cell_id, cell.quantity))
         return cells_of_product
 
+
 class StockManager:
-    def __init__(self,warehouse: Warehouse):
+    def __init__(self, warehouse: Warehouse):
         self.warehouse = warehouse
 
-    def check_stock(self,product: Product) -> int:
+    def check_stock(self, product: Product) -> int:
         return self.warehouse.get_stock(product)
 
-    def reserve (self,product: Product,quantity: int) -> bool:
+    def reserve(self, product: Product, quantity: int) -> bool:
         if self.check_stock(product) < quantity:
             return False
         for shelf in self.warehouse.shelves:
             if quantity == 0:
                 break
-            reserved = min(shelf.all_available(product),quantity)
-            shelf.get_product(product,reserved)
+            reserved = min(shelf.all_available(product), quantity)
+            shelf.get_product(product, reserved)
             quantity -= reserved
         return quantity == 0
 
-    def replenish(self,product: Product,quantity: int) -> bool:
+    def replenish(self, product: Product, quantity: int) -> bool:
         remaining = quantity
         for shelf in self.warehouse.shelves:
             for cell in shelf.cells:
                 if cell.product and cell.product.product_id == product.product_id:
                     space = cell.max_quantity - cell.quantity
-                    to_add = min(space,remaining)
+                    to_add = min(space, remaining)
                     if to_add > 0:
                         print(cell.cell_id, to_add)
-                        cell.store(product,to_add)
+                        cell.store(product, to_add)
                         remaining -= to_add
                         if remaining == 0:
                             return True
@@ -162,49 +180,47 @@ class StockManager:
             for cell in shelf.cells:
                 if cell.product is None and cell.is_available():
                     space = cell.max_quantity - cell.quantity
-                    to_add = min(space,remaining)
+                    to_add = min(space, remaining)
                     if to_add > 0:
-                        cell.store(product,to_add)
+                        cell.store(product, to_add)
                         remaining -= to_add
                         if remaining == 0:
                             return True
         return remaining == 0
 
+
 """Classes for Users System"""
 class User:
-    def __init__(self,user_id: int, name: str, surname: str, email: str, password: str):
+    def __init__(self, user_id: int, name: str, surname: str, email: str, password: str,address_name: str):
         self.user_id = user_id
         self.name = name
         self.surname = surname
-        self.addresses = []
+        self.address = address_name
         self.email = email
         self.password = password
         self.cart = Cart(self)
         self.orders = []
 
-    def add_address(self, address):
-        self.addresses.append(address)
-
     def make_an_order(self):
         if self.cart.is_empty():
             print("Корзина пустая")
             return None
-        order = Order(self,self.cart)
+        order = Order(self, self.cart)
         self.orders.append(order)
         self.cart.clear()
         return order
 
 class Cart:
-    def __init__(self,user: User):
+    def __init__(self, user: User):
         self.user = user
         self.items = {}
         self.cost = 0
 
-    def add_item(self,product: Product, quantity: int):
-        self.items[product.product_id] = self.items.get(product.product_id,0) + quantity
+    def add_item(self, product: Product, quantity: int):
+        self.items[product.product_id] = self.items.get(product.product_id, 0) + quantity
         self.cost += quantity * product.price
 
-    def remove_item(self,product: Product):
+    def remove_item(self, product: Product):
         if product.product_id in self.items:
             self.cost -= self.items[product.product_id].items()[0] * product.price
             del self.items[product.product_id]
@@ -219,15 +235,13 @@ class Cart:
     def get_cost(self):
         return self.cost
 
+
 class Order:
-    def __init__(self,user: User,cart: Cart):
+    def __init__(self, user: User, cart: Cart):
         self.user = user
         self.items = cart.items
         self.status = "Создан"
         self.cost = cart.cost
 
-
-    def update_status(self,new_status:str):
+    def update_status(self, new_status: str):
         self.status = new_status
-
-
